763fce0f83de04329f1ba2714cde76b75b1c105a
Yew Long <yewlong42@gmail.com>
2013-02-22 15:28:15 -0500

    Simplified getAlpha and getBeta to take only 1 integer argument

--- CS-6238-Ass1/src/passHardenning/InstructionTable.java
+++ CS-6238-Ass1/src/passHardenning/InstructionTable.java
@@ -4,31 +4,55 @@ import java.math.BigInteger;
 
 public class InstructionTable {
 	Utilities util;
+	BigInteger[] polynomial; 
+	BigInteger q;
+	BigInteger[] alpha;
+	BigInteger[] beta;
+	int m;
 	
 	public InstructionTable(){
-		//TODO : find a way to point this.util to main's copy of the utilities
+		//TODO : find a way to point this.util to main's copy of the utilities main.util
+		//  find a way to point this.polynomial to main's copy in main.init.polynomial
+		// also for this.q to point to main.init.q
+		// also for this.m to copy from main.init.m 
+		alpha = new BigInteger[m];
+		beta = new BigInteger[m];
 		
 	}
-	private void decrypt(String pass){
+	private void readFromDisk(String pass){
 		
 	}
 	
-	private void endrypt(String pass){
+	private void writeToDisk(String pass){
 		
+	}
+	public void buildInstrTable(BigInteger r, String pwd){
+		for(int i = 0; i < m; i++){
+			alpha[i] = calculateAlpha(r, i, pwd);
+			beta[i] = calculateBeta(r, i, pwd);
+		}
+	}
+	
+	public BigInteger getAlpha(int i){
+		return alpha[i];		
+	}
+	public BigInteger getBeta(int i){
+		return beta[i];
 	}
 	
 	//r is unique to each user, see page 74 of Monrose section 5.1 item 1
-	public BigInteger getAlpha(BigInteger r, int i, BigInteger q, String pwd){
-		return calculateAlphaBeta(r, i * 2 , q, pwd);
+	private BigInteger calculateAlpha(BigInteger r, int i, String pwd){
+		return calculateAlphaBeta(r, i * 2 , pwd);
 	}
 	
 	//wrapper for calculateAlphaBeta() 
-	public BigInteger getBeta(BigInteger r, int i, BigInteger q, String pwd){
-		return calculateAlphaBeta(r, i * 2 + 1, q, pwd);
+	private BigInteger calculateBeta(BigInteger r, int i, String pwd){
+		return calculateAlphaBeta(r, i * 2 + 1, pwd);
 		
 	}
-	private BigInteger calculateAlphaBeta(BigInteger r, int input, BigInteger q, String pwd){
-		BigInteger y = util.P(r, input , q); //this is y_{ai}^0 in Monrose section 5.1 item 2
+	private BigInteger calculateAlphaBeta(BigInteger r, int input, String pwd){
+		BigInteger randomizedX = util.P(r, input , q);
+		BigInteger y = util.evaluatePoly(polynomial, r, randomizedX); // this is y_{ai}^0/1 in Monrose section 5.1 item 2
 		BigInteger g = util.G(pwd, r, input, q); 
 		return y.add(g).mod(q);
 	}
